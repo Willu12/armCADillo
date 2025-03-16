96d0ca4644b971e9073a116e7891d40e7974cfb5
Willu12 <boryskurdek@gmail.com>
2025-03-16 23:04:47 +0100

    removed legacy code

--- src/backend/include/triangle.hpp
+++ src/backend/include/triangle.hpp
@@ -62,42 +62,4 @@ public:
     // VBOs) when it's not directly necessary.
     glBindVertexArray(0);
   }
-
-private:
-  /*
-    void createSharders() {
-      vertexShader = glCreateShader(GL_VERTEX_SHADER);
-      glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-      glCompileShader(vertexShader);
-
-      fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-      glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-      glCompileShader(fragmentShader);
-
-     // shaderProgram = glCreateProgram();
-      glAttachShader(shaderProgram, vertexShader);
-      glAttachShader(shaderProgram, fragmentShader);
-      glLinkProgram(shaderProgram);
-
-      glUseProgram(shaderProgram);
-
-      glDeleteShader(vertexShader);
-      glDeleteShader(fragmentShader);
-    }
-    const char *vertexShaderSource =
-        "#version 330 core\n"
-        "layout (location = 0) in vec3 aPos;\n"
-        "void main()\n"
-        "{\n"
-        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-        "}\0";
-
-    const char *fragmentShaderSource =
-        "#version 330 core\n"
-        "out vec4 FragColor;\n"
-        "void main()\n"
-        "{\n"
-        "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-        "}\n\0";
-        */
 };
