c3fb44139d805998e75027e5382f95e0f8edfae3
Willu12 <boryskurdek@gmail.com>
2025-11-01 12:34:01 +0100

    dump

--- src/paths/heightMapGenerator.cpp
+++ src/paths/heightMapGenerator.cpp
@@ -1,5 +1,12 @@
 #include "heightMapGenerator.hpp"
+#include "heightMap.hpp"
+
+static constexpr float kBaseHeight = 5.f;
+static constexpr uint32_t kBaseDivisions = 1500;
 
 HeightMap HeightMapGenerator::generateHeightMap(const Model &model) {
-  return HeightMap{Divisions{.x_ = 0, .y_ = 0}, 5.f};
+
+  HeightMap height_map(Divisions{.x_ = kBaseDivisions, .y_ = kBaseDivisions},
+                       kBaseHeight);
+  // return HeightMap{Divisions{.x_ = 0, .y_ = 0}, 5.f};
 }
